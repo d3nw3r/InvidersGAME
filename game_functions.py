2440424287b9002b1ad887a6dd043e2e478b9134
d3nw3r <35667244+d3nw3r@users.noreply.github.com>
2019-03-17 22:07:49 +0200

    17/03/2019

--- game_functions.py
+++ game_functions.py
@@ -2,24 +2,30 @@ import sys
 import pygame
 
 
+def chek_keydown_event(event, ship):
+    """Реагирует на нажатие клавиш"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def chek_keyup_event(event, ship):
+    """Реагирует на отпускание клавиши"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 def chek_events(ship):
     """обробатывает нажатие клавишы и события от мыши"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-
+            chek_keydown_event(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            chek_keyup_event(event, ship)
+
 
 def update_screen(ai_settings, screen, ship):
     # заполняем єкран цветом bg_color
